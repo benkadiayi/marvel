999d0ab754074b2a403a5d4e7a7676fe031e6890
benkadiayi <60475925+benkadiayi@users.noreply.github.com>
2021-10-20 17:10:35 -0500

    Update config.py

--- config.py
+++ config.py
@@ -7,5 +7,5 @@ load_dotenv(os.path.join(basedir, '.env'))
 class config():
     
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'You will never guess'
-    SQLALCHEMY_DATABASE_URL = 'postgresql+psycopg2://postgres:Excellence@127.0.0.1:5432/marvel-collection' or 'sqlite:///' + os.path.join(basedir, 'app.db')
+    SQLALCHEMY_DATABASE_URI ='sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False 
